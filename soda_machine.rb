edeb9adb5b1b28b38aa73e821fd2cabb271810d8
trefong <tre.fong1@gmail.com>
2016-05-09 14:28:16 -0700

    Complete second checkpoint challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,22 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.find{|soda|
+      soda.brand == soda_brand
+    }
   end
 
   def sell(soda_brand)
+    soda = find_soda(soda_brand)
+    if soda != nil
+      @cash += soda.price
+      @sodas.delete(soda)
+    end
+
   end
 
 end
